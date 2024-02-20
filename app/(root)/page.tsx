93a42480a820bd75434b44c72839bd996b45fdb0
sfayzer <aymenkhaze3.9999@gmail.com>
2024-02-20 23:30:10 +0100

    first commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -28,7 +28,7 @@ function Home() {
         </div>
       </section>
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-      <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+      <h2 className="h2-bold"> <br /> Search</h2>
       <div className="flex w-full flex-col gap-5 md:flex-row">
           Search
           CategoryFilter
